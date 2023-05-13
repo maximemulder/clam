a189c8388f145a2cf0a58029822c29ab3d15d6a9
Maxime Mulder <maxime-mulder@outlook.com>
2023-05-13 19:32:37 -0400

    ugly and wrong type checking

--- src/typecheck.ml
+++ src/typecheck.ml
@@ -12,6 +12,12 @@ end
 
 open Monad.Monad(Monad.ReaderMonad(Reader))
 
+let compare_lists compare list other =
+  if List.compare_lengths list other != 0 then false else
+  let pairs = List.combine list other in
+  let compare_pair = fun (a, b) -> compare a b in
+  List.for_all compare_pair pairs
+
 let rec get_type param context =
   match List.find_opt (fun other -> (fst other) == param) context.binds with
   | Some pair -> snd pair
@@ -20,33 +26,71 @@ let rec get_type param context =
 let rec is_subtype_of (type': Model.type') (other: Model.type') =
   if other == TypeAny then true else
   match type' with
-  | TypeAny -> false
-  | TypeVoid -> other == TypeVoid
-  | TypeBool -> other == TypeBool
-  | TypeInt -> other == TypeInt
-  | TypeChar -> other == TypeChar
+  | TypeAny    -> false
+  | TypeVoid   -> other == TypeVoid
+  | TypeBool   -> other == TypeBool
+  | TypeInt    -> other == TypeInt
+  | TypeChar   -> other == TypeChar
   | TypeString -> other == TypeString
-  | TypeTuple types -> is_tuple_subtype_of types other
-  | TypeRecord attrs -> is_record_subtype_of attrs other
+  | TypeVar         param          -> other == (TypeVar param) || is_subtype_of param.Model.type_param_type other
+  | TypeAbsExpr     (params, expr) -> is_abs_expr_subtype_of params expr other
+  | TypeAbsExprType (params, expr) -> is_abs_expr_type_subtype_of params expr other
+  | TypeTuple       types          -> is_tuple_subtype_of types other
+  | TypeRecord      attrs          -> is_record_subtype_of attrs other
+  | TypeInter      (left, right)   -> is_inter_subtype_of left right other
+  | TypeUnion      (left, right)   -> is_union_subtype_of left right other
+  | TypeAbs        (params, type') -> is_abs_subtype_of params type' other
+  | TypeApp        (type', args)   -> is_app_subtype_of type' args other
+
+and is_abs_expr_subtype_of params expr other =
+  match other with
+  | Model.TypeAbsExpr (other_params, other_expr) ->
+    compare_lists is_subtype_of other_params params && is_subtype_of expr other_expr
+  | _ -> false
+
+and is_abs_expr_type_subtype_of params expr other =
+  match other with
+  | Model.TypeAbsExprType (other_params, other_expr) ->
+    compare_lists (==) other_params params && is_subtype_of expr other_expr
   | _ -> false
 
 and is_tuple_subtype_of (types: Model.type' list) (other: Model.type') =
   match other with
-  | TypeTuple others ->
-    let length_types = List.length types in
-    let length_others = List.length others in
-    if length_types != length_others then false else
-    let pairs = List.combine types others in
-    List.for_all (fun (type', other) -> is_subtype_of type' other) pairs
+  | Model.TypeTuple others ->
+    compare_lists is_subtype_of types others
   | _ -> false
 
-and is_record_subtype_of (attrs: Model.attr_type NameMap.t) (other: Model.type') =
+and is_record_subtype_of attrs other =
   match other with
-  | TypeRecord others ->
+  | Model.TypeRecord others ->
     NameMap.for_all (fun name other -> match NameMap.find_opt name attrs with
     | Some attr -> is_subtype_of attr.Model.attr_type other.Model.attr_type
     | None -> false
     ) others
+  | _ -> false
+
+and is_inter_subtype_of left right other =
+  match other with
+  | Model.TypeInter (other_left, other_right) ->
+    is_subtype_of left other_left && is_subtype_of right other_right
+  | _ -> false
+
+and is_union_subtype_of left right other =
+  match other with
+  | Model.TypeUnion (other_left, other_right) ->
+    is_subtype_of left other_left && is_subtype_of right other_right
+  | _ -> false
+
+and is_abs_subtype_of params type' other =
+  match other with
+  | Model.TypeAbs (other_params, other_expr) ->
+    compare_lists (==) other_params params && is_subtype_of type' other_expr
+  | _ -> false
+
+and is_app_subtype_of type' args other =
+  match other with
+  | Model.TypeApp (other_type, other_args) ->
+    is_subtype_of type' other_type && compare_lists (==) args other_args
   | _ -> false
 
 let require_subtype (type': Model.type') (constraint': Model.type') =
@@ -57,10 +101,20 @@ let require_subtype (type': Model.type') (constraint': Model.type') =
 let rec promote (type': Model.type') =
   match type' with
   | TypeVar param -> get_type param
+  | TypeAny    -> return Model.TypeAny
+  | TypeVoid   -> return Model.TypeVoid
+  | TypeBool   -> return Model.TypeBool
+  | TypeInt    -> return Model.TypeInt
+  | TypeChar   -> return Model.TypeChar
+  | TypeString -> return Model.TypeString
   | TypeAbsExpr (params, expr) ->
     let* params = map_list promote params in
     let* expr = promote expr in
     return (Model.TypeAbsExpr (params, expr))
+  | TypeAbsExprType (params, type') ->
+    let* params = map_list promote_param params in
+    let* type' =  promote type' in
+    return (Model.TypeAbs (params, type'))
   | TypeTuple types ->
     let* types = map_list promote types in
     return (Model.TypeTuple types)
@@ -80,7 +134,6 @@ let rec promote (type': Model.type') =
     let* type' =  promote type' in
     return (Model.TypeAbs (params, type'))
   | TypeApp (type', args) -> promote_app type' args
-  | _ -> return type'
 
 and promote_app type' args context =
   match type' with
@@ -109,8 +162,6 @@ let require_constraint constraint' type' =
     let* type' = promote type' in
     return (require_subtype type' constraint')
   | None -> return ()
-
-
 
 let rec get_expr_type expr =
   match expr with
