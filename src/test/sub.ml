2c43799e28ad08d17b39075c6760f3b53eb963f0
Maxime Mulder <maxime-mulder@outlook.com>
2023-08-31 22:37:23 +0200

    add some testing

--- src/test/sub.ml
+++ src/test/sub.ml
@@ -1,10 +1,28 @@
 open Vars
 
-let assert_sub type' constraint' =
-  assert (Clam.TypingSub.is_subtype type' constraint' Clam.TypingContext.context_empty)
+let test_sub name sub sup _ =
+  let result = Clam.TypingSub.is_subtype sub sup Clam.TypingContext.context_empty in
+  Alcotest.(check bool) name result true
 
-;; assert_sub a a
-;; assert_sub a (union a b)
-;; assert_sub (union a b) (union a b)
-;; assert_sub (inter a b) (union b a)
-;; (* assert_sub (union (inter a b) (inter a c)) (inter a (union b c)) *)
+let case_sub name sub sup =
+  let test = test_sub name sub sup in
+  Alcotest.test_case name Alcotest.(`Quick) test
+
+let tests = [
+  case_sub "A : A"
+    a a;
+  case_sub "A : A | B"
+    a (union a b);
+  case_sub "A | B : A | B"
+    (union a b) (union a b);
+  case_sub "A & B : A & B"
+    (inter a b) (union b a);
+  case_sub "(A & B) | (A & C) : A & (B | C)"
+    (union (inter a b) (inter a c)) (inter a (union b c));
+  case_sub "A & (B | C) : (A & B) | (A & C)"
+    (inter a (union b c)) (union (inter a b) (inter a c));
+];;
+
+Alcotest.run "clam" [
+  "sets", tests
+]
