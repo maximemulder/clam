7e554ae2be315ad090c7112a551e3c6441cdcad8
Maxime Mulder <maxime-mulder@outlook.com>
2024-02-28 21:22:56 -0500

    better inference variables naming

--- src/infer/rename.ml
+++ src/infer/rename.ml
@@ -16,55 +16,65 @@ let rec index_to_name index =
 let index_to_name index =
   "'" ^ index_to_name index
 
-let rec rename index type' =
-  rename_union index type'
+include Util.Monad.Monad(Util.Monad.StateMonad(struct
+  type s = int
+end))
 
-and rename_union index union =
-  { union = List.map (rename_inter index) union.union }
+let rec rename type' =
+  rename_union type'
 
-and rename_inter index inter =
-  { inter = List.map (rename_base index) inter.inter }
+and rename_union union =
+  let* union = list_map rename_inter union.union in
+  return { union }
 
-and rename_base index type' =
+and rename_inter inter =
+  let* inter = list_map rename_base inter.inter in
+  return { inter }
+
+and rename_base type' =
   match type' with
   | Top | Bot | Unit | Bool | Int | String | Var _ ->
-    type'
+    return type'
   | Tuple tuple ->
-    let elems = List.map (rename index) tuple.elems in
-    Tuple { elems }
+    let* elems = list_map rename tuple.elems in
+    return (Tuple { elems })
   | Record record ->
-    let attrs = Util.NameMap.map (rename_attr index) record.attrs in
-    Record { attrs }
+    let* attrs = map_map rename_attr record.attrs in
+    return (Record { attrs })
   | Lam lam ->
-    let param = rename index lam.param in
-    let ret = rename index lam.ret in
-    Lam { param; ret }
+    let* param = rename lam.param in
+    let* ret = rename lam.ret in
+    return (Lam { param; ret })
   | Univ univ ->
-    if Str.string_match pattern univ.param.bind.name 0 then
-      let param = rename_param (index + 1) univ.param in
-      let ret = rename (index + 1) univ.ret in
-      let bind = { Abt.name = index_to_name index } in
-      let ret = Type.rename ret param.bind bind in
-      let param = { param with bind } in
-      Univ { param; ret }
-    else
-      let param = rename_param index univ.param in
-      let ret = rename index univ.ret in
-      Univ { param; ret }
+    let* univ: univ = rename_infer univ in
+    let* param = rename_param univ.param in
+    let* ret = rename univ.ret in
+    return (Univ { param; ret })
   | Abs abs ->
-    let param = rename_param index abs.param in
-    let body = rename index abs.body in
-    Abs { param; body }
+    let* param = rename_param abs.param in
+    let* body = rename abs.body in
+    return (Abs { param; body })
   | App app ->
-    let abs = rename index app.abs in
-    let arg = rename index app.arg in
-    App { abs; arg }
+    let* abs = rename app.abs in
+    let* arg = rename app.arg in
+    return (App { abs; arg })
 
-and rename_attr index attr =
-  { attr with type' = rename index attr.type' }
+and rename_attr attr =
+  let* type' = rename attr.type' in
+  return { attr with type' }
 
-and rename_param index param =
-  { param with bound = rename index param.bound }
+and rename_param param =
+  let* bound = rename param.bound in
+  return { param with bound }
+
+and rename_infer univ i =
+  if Str.string_match pattern univ.param.bind.name 0 then
+    let bind = { Abt.name = index_to_name i } in
+    let param = { univ.param with bind } in
+    let ret = Type.rename univ.ret univ.param.bind bind in
+    { param; ret }, i + 1
+  else
+    univ, i
 
 let rename type' =
-  rename 0 type'
+  rename type' 0 |> fst
