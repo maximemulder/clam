783cbb9daf9d5d84106092045efebe9a38c1dd84
Maxime Mulder <maxime-mulder@outlook.com>
2024-03-09 10:38:24 -0500

    remove unused position from type constraining

--- src/infer/constrain.ml
+++ src/infer/constrain.ml
@@ -88,112 +88,112 @@ let get_infer_var_sup sup =
   | _ ->
     return None
 
-let rec constrain pos (sub: Type.type') (sup: Type.type') =
-  constrain_union_1 pos sub sup
+let rec constrain (sub: Type.type') (sup: Type.type') =
+  constrain_union_1 sub sup
 
-and constrain_union_1 pos sub sup =
-  list_all (fun sub -> constrain_union_2 pos sub sup) sub.union
+and constrain_union_1 sub sup =
+  list_all (fun sub -> constrain_union_2 sub sup) sub.union
 
-and constrain_union_2 pos sub sup =
+and constrain_union_2 sub sup =
   let* sub_var = get_infer_var_sub sub in
   match sub_var with
   | Some sub_var when List.length sup.union > 1 ->
-    constrain_sub_var pos sub_var sup
+    constrain_sub_var sub_var sup
   | _ ->
-    list_any (constrain_inter_1 pos sub) sup.union
+    list_any (constrain_inter_1 sub) sup.union
 
-and constrain_inter_1 pos sub sup =
-  list_all (constrain_inter_2 pos sub) sup.inter
+and constrain_inter_1 sub sup =
+  list_all (constrain_inter_2 sub) sup.inter
 
-and constrain_inter_2 pos sub sup =
+and constrain_inter_2 sub sup =
   let* sup_var = get_infer_var_sup sup in
   match sup_var with
   | Some sup_var when List.length sub.inter > 1 ->
     let sub = { Type.union = [sub] } in
-    constrain_sup_var pos sup_var sub
+    constrain_sup_var sup_var sub
   | _ ->
-    list_any (fun sub -> constrain_base pos sub sup) sub.inter
+    list_any (fun sub -> constrain_base sub sup) sub.inter
 
-and constrain_base pos sub sup =
+and constrain_base sub sup =
   let* state = get_state in
   match sub, sup with
   | Var sub_var, Var sup_var when is_infer sub_var.bind state && is_infer sup_var.bind state ->
-    let* sub_res = constrain_sub_var pos sub_var (Type.base sup) in
-    let* sup_res = constrain_sup_var pos sup_var (Type.base sub) in
+    let* sub_res = constrain_sub_var sub_var (Type.base sup) in
+    let* sup_res = constrain_sup_var sup_var (Type.base sub) in
     return (sub_res && sup_res)
   | Var sub_var, _ when is_infer sub_var.bind state ->
     let sup = Type.base sup in
-    constrain_sub_var pos sub_var sup
+    constrain_sub_var sub_var sup
   | _, Var sup_var when is_infer sup_var.bind state ->
     let sub = Type.base sub in
-    constrain_sup_var pos sup_var sub
+    constrain_sup_var sup_var sub
   | Tuple sub_tuple, Tuple sup_tuple ->
-    constrain_tuple pos sub_tuple sup_tuple
+    constrain_tuple sub_tuple sup_tuple
   | Record sub_record, Record sup_record ->
-    constrain_record pos sub_record sup_record
+    constrain_record sub_record sup_record
   | Lam sub_lam, Lam sup_lam ->
-    constrain_lam pos sub_lam sup_lam
+    constrain_lam sub_lam sup_lam
   | Univ sub_univ, _ ->
     let* var = make_var in
-    let* lower = constrain pos sub_univ.param.lower var in
-    let* upper = constrain pos var sub_univ.param.upper in
+    let* lower = constrain sub_univ.param.lower var in
+    let* upper = constrain var sub_univ.param.upper in
     let* ctx = get_context in
     let ret = Type.System.substitute ctx sub_univ.ret sub_univ.param.bind var in
-    let* ret = constrain pos ret (Type.base sup) in
+    let* ret = constrain ret (Type.base sup) in
     return (lower && upper && ret)
   | _, Univ sup_univ ->
     with_type sup_univ.param.bind sup_univ.param.lower sup_univ.param.upper
-      (constrain pos (Type.base sub) sup_univ.ret)
+      (constrain (Type.base sub) sup_univ.ret)
   | _, _ ->
     let* ctx = get_context in
     let result = Type.System.isa ctx (Type.base sub) (Type.base sup) in
     return result
 
-and constrain_sub_var pos sub_var sup =
+and constrain_sub_var sub_var sup =
   let* cond = is_direct_sup sub_var.bind sup in
   if not cond then
     let* entry = get_var_entry sub_var.bind in
     let* () = levelize sup entry.level in
     let* () = update_var_upper sub_var.bind sup in
     let* sub_lower = get_var_lower sub_var.bind in
-    constrain pos sub_lower sup
+    constrain sub_lower sup
   else
     (* TODO: Handle cycle *)
     return true
 
-and constrain_sup_var pos sup_var sub =
+and constrain_sup_var sup_var sub =
   let* cond = is_direct_sub sup_var.bind sub in
   if not cond then
     let* entry = get_var_entry sup_var.bind in
     let* () = levelize sub entry.level in
     let* () = update_var_lower sup_var.bind sub in
     let* sup_upper = get_var_upper sup_var.bind in
-    constrain pos sub sup_upper
+    constrain sub sup_upper
   else
     (* TODO: Handle cycle *)
     return true
 
-and constrain_tuple pos sub_tuple sup_tuple =
+and constrain_tuple sub_tuple sup_tuple =
   List.combine sub_tuple.elems sup_tuple.elems
-  |> list_all (fun (sub, sup) -> constrain pos sub sup)
+  |> list_all (fun (sub, sup) -> constrain sub sup)
 
-and constrain_record pos sub_record sup_record =
-  map_all (fun sup_attr -> constrain_record_attr pos sub_record sup_attr) sup_record.attrs
+and constrain_record sub_record sup_record =
+  map_all (fun sup_attr -> constrain_record_attr sub_record sup_attr) sup_record.attrs
 
-and constrain_record_attr pos sub_record sup_attr =
+and constrain_record_attr sub_record sup_attr =
   match Util.NameMap.find_opt sup_attr.label sub_record.attrs with
   | Some sub_attr ->
-    constrain pos sub_attr.type' sup_attr.type'
+    constrain sub_attr.type' sup_attr.type'
   | None ->
     return true
 
-and constrain_lam pos sub_abs sup_abs =
-  let* param = constrain pos sup_abs.param sub_abs.param in
-  let* ret = constrain pos sub_abs.ret sup_abs.ret in
+and constrain_lam sub_abs sup_abs =
+  let* param = constrain sup_abs.param sub_abs.param in
+  let* ret = constrain sub_abs.ret sup_abs.ret in
   return (param && ret)
 
 let constrain pos sub sup =
-  let* result = constrain pos sub sup in
+  let* result = constrain sub sup in
   if result then
     return ()
   else
