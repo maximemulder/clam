3aa1abda600fd19b16d2562726488b44b902a86c
Maxime Mulder <maxime-mulder@outlook.com>
2024-05-07 23:08:38 -0400

    wip

--- src/test/cases/is.ml
+++ src/test/cases/is.ml
@@ -1,4 +1,8 @@
-open Vars
+open Type2
+open Type2.Display
+open Type2.Build
+open Type2.Build.Default
+open Util.Func
 
 let test ctx left right (_: unit) =
   System.is left right ctx |> fst
@@ -14,7 +18,7 @@ let case left right expect ctx =
 
 let case_var name bound case expect ctx =
   let bind = { Abt.name } in
-  let ctx = { ctx with Context.rigids = { bind; lower = Type.bot; upper = bound } :: ctx.Context.rigids } in
+  let ctx = { ctx with Context.rigids = { bind; lower = bot; upper = bound } :: ctx.Context.rigids } in
   let var = var bind in
   case var expect ctx
 
@@ -36,47 +40,47 @@ let tests = [
   case a a;
 
   (* unions *)
-  case a (union [a; a]);
-  case (union [a; a]) a;
-  case (union [a; a]) (union [a; a]);
-  case (union [a; b]) (union [a; b]);
-  case (union [a; b]) (union [b; a]);
-  case (union [a; union [b; c]]) (union [a; union [b; c]]);
-  case (union [a; union [b; c]]) (union [union [a; b]; c]);
-  case (union [union [a; b]; c]) (union [a; union [b; c]]);
-  case (union [top; a]) top;
-  case top (union [top; a]);
-  case (union [a; union [b; ea]]) (union [a; b]);
-  case (union [a; b]) (union [a; union [b; ea]]);
+  case a (union a a);
+  case (union a a) a;
+  case (union a a) (union a a);
+  case (union a b) (union a b);
+  case (union a b) (union b a);
+  case (union a (union b c)) (union a (union b c));
+  case (union a (union b c)) (union (union a b) c);
+  case (union (union a b) c) (union a (union b c));
+  case (union top a) top;
+  case top (union top a);
+  case (union a (union b a1)) (union a b);
+  case (union a b) (union a (union b a1));
 
   (* intersections *)
-  case a (inter [a; a]);
-  case (inter [a; a]) a;
-  case (inter [a; a]) (inter [a; a]);
-  case (inter [a; b]) (inter [a; b]);
-  case (inter [a; b]) (inter [b; a]);
-  case (inter [top; a]) a;
-  case a (inter [top; a]);
-  case (inter [a; inter [b; ea]]) (inter [ea; b]);
-  case (inter [ea; b]) (inter [a; inter [b; ea]]);
+  case a (inter a a);
+  case (inter a a) a;
+  case (inter a a) (inter a a);
+  case (inter a b) (inter a b);
+  case (inter a b) (inter b a);
+  case (inter top a) a;
+  case a (inter top a);
+  case (inter a (inter b a1)) (inter a1 b);
+  case (inter a1 b) (inter a (inter b a1));
 
   (* distributivity *)
-  case (union [inter [a; b]; inter [a; c]]) (inter [a; union [b; c]]);
-  case (inter [a; union [b; c]]) (union [inter [a; b]; inter [a; c]]);
+  case (union (inter a b) (inter a c)) (inter a (union b c));
+  case (inter a (union b c)) (union (inter a b) (inter a c));
 
   (* meets *)
-  case (inter [lam a c; lam b c]) (lam (union [a; b]) c);
-  case (lam (union [a; b]) c) (inter [lam a c; lam b c]);
-  case (inter [lam a b; lam a c]) (lam a (inter [b; c]));
-  case (lam a (inter [b; c])) (inter [lam a b; lam a c]);
+  case (inter (lam a c) (lam b c)) (lam (union a b) c);
+  case (lam (union a b) c) (inter (lam a c) (lam b c));
+  case (inter (lam a b) (lam a c)) (lam a (inter b c));
+  case (lam a (inter b c)) (inter (lam a b) (lam a c));
 
   (* universal abstractions *)
-  case (univ "A" top (inline a)) (univ "A" top (inline a));
-  case (univ "A" top id) (univ "A" top id);
-  case (univ "A" top id) (univ "B" top id);
+  case (univ_0 "A" (const a)) (univ_0 "A" (const a));
+  case (univ_0 "A" id) (univ_0 "A" id);
+  case (univ_0 "A" id) (univ_0 "B" id);
 
-  case (univ "A" top (fun a -> (univ "B" (inter [a; int]) (fun b -> (lam b (tuple [a; b])))))) (univ "A" int (fun a -> (lam a (tuple [a; a]))));
-  case (univ "A" top (fun a -> (univ "B" a (fun b -> (tuple [a; b]))))) (univ "A" top (fun a -> (tuple [a; a])));
+  case (univ_0 "A" (fun a -> (univ "B" bot (inter a int) (fun b -> (lam b (tuple [a; b])))))) (univ "A" bot int (fun a -> (lam a (tuple [a; a]))));
+  case (univ_0 "A" (fun a -> (univ "B" bot a (fun b -> tuple [a; b])))) (univ_0 "A" (fun a -> (tuple [a; a])));
 ]
 |> List.map (fun case -> case true ctx)
 
@@ -97,36 +101,36 @@ let tests_not = [
 
   (* variables *)
   case a b;
-  case ea a;
-  case a ea;
-  case ea fa;
+  case a1 a;
+  case a a1;
+  case a1 a2;
 
   (* unions *)
-  case a (union [a; b]);
-  case (union [a; b]) a;
-  case (union [top; a]) a;
-  case a (union [top; a]);
-  case (union [a; union [b; ea]]) (union [ea; b]);
-  case (union [ea; b]) (union [a; union [b; ea]]);
+  case a (union a b);
+  case (union a b) a;
+  case (union top a) a;
+  case a (union top a);
+  case (union a (union b a1)) (union a1 b);
+  case (union a1 b) (union a (union b a1));
 
   (* interesections *)
-  case a (inter [a; b]);
-  case (inter [a; b]) a;
-  case (inter [top; a]) top;
-  case top (inter [top; a]);
-  case (inter [a; inter [b; ea]]) (inter [a; b]);
-  case (inter [a; b]) (inter [a; inter [b; ea]]);
+  case a (inter a b);
+  case (inter a b) a;
+  case (inter top a) top;
+  case top (inter top a);
+  case (inter a (inter b a1)) (inter a b);
+  case (inter a b) (inter a (inter b a1));
 
   (* meets *)
-  case (inter [lam a c; lam b d]) (lam (union [a; b]) (inter [c; d]));
-  case (lam (union [a; b]) (inter [c; d])) (inter [lam a c; lam b d]);
+  case (inter (lam a c) (lam b d)) (lam (union a b) (inter c d));
+  case (lam (union a b) (inter c d)) (inter (lam a c) (lam b d));
 
   (* ambiguous names *)
   case_var "A" top (fun a1 -> case_var "A" top (fun a2 -> case a1 a2));
-  case (univ "A" top (inline a)) (univ "A" top id);
-  case (univ "A" top id) (univ "A" top (inline a));
+  case (univ_0 "A" (const a)) (univ_0 "A" id);
+  case (univ_0 "A" id) (univ_0 "A" (const a));
 
   (* type abstractions and variables *)
-  case_var "T" (abs "X" top id) (fun t1 -> case_var "T" (abs "X" top id) (fun t2 -> case (app t1 top) (app t2 top)));
+  case_var "T" (abs_0 "X" id) (fun t1 -> case_var "T" (abs_0 "X" id) (fun t2 -> case (app t1 top) (app t2 top)));
 ]
 |> List.map (fun case -> case false ctx)
