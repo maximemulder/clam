a1c28b3678608cc09d129c11cf4fcff502e513dc
Maxime Mulder <maxime-mulder@outlook.com>
2024-07-27 01:35:09 -0400

    wip

--- src/util/monad2.ml
+++ src/util/monad2.ml
@@ -21,38 +21,93 @@ module Monad (M: MONAD) = struct
   module Monad = Util(M)
 end
 
-module Identity = Monad(struct
-  type 'a t = 'a
+module Identity = struct
+    type 'a t = 'a
 
-  let return a =
-    a
+    let return a =
+      a
 
-  let bind m f =
-    f m
-
-end)
+    let bind m f =
+      f m
+end
 
 module type RESULT = sig
   type error
 end
 
-module Result (R: RESULT) = Monad(struct
-  type 'a t = ('a, R.error) result
+(* sig
+  type 'a t
 
-  let return a = Ok a
+  val return : 'a -> 'a t
+  val bind : 'a t -> ('a -> 'b t) -> 'b t
+end *)
 
-  let bind m f =
+(* module ResultT (M: MONAD) (R: RESULT) = Monad(struct
+  type 'a t = ('a M.t, R.error) result
+
+  let return (a: 'a) : ('a M.t, 'b) result =
+    Ok (M.return a)
+
+  let bind (m: ('a M.t, 'b) result) (f: 'a -> ('c M.t, 'b) result) : ('c M.t, 'b) result =
+    match m with
+    | Ok v ->
+      let e = f v in
+      let d = M.bind v (fun x ->
+        let e = f x in
+        failwith "A"
+        ) in
+      let a = f (return v) in
+      let b = return ( M.bind v f) in
+      b
+    | Error e ->
+      Error e
+
+  (* let bind m f =
+    match m with
+    | Ok v ->
+      failwith "A"
+    | Error e ->
+      Error e
+    (*
     match m with
     | Ok  v -> f v
     | Error e -> Error e
+    ) *) *)
 
-end)
+end) *)
+
+module ResultT (M: MONAD) (R: RESULT) = struct
+  module Base = struct
+    type 'a t = ('a, R.error) result M.t
+
+    let return a =
+      M.return (Ok a)
+
+    let bind m f =
+      M.bind m (fun m ->
+        match m with
+        | Ok v ->
+          f v
+        | Error e ->
+          M.return (Error e)
+      )
+  end
+
+  include Monad(Base)
+  include Base
+
+  let fail e =
+    M.return (Error e)
+
+end
+
+module Result = ResultT(Identity)
 
 module type READER = sig
   type reader
 end
 
-module Reader (R: READER) = Monad(struct
+module ReaderT (R: READER) = Monad(struct
   type 'a t = R.reader -> 'a
 
   let return a _ =
@@ -70,14 +125,18 @@ module type STATE = sig
   type state
 end
 
-module State (S: STATE) = Monad(struct
-  type 'a t = S.state -> ('a * S.state)
+module State (S: STATE) = struct
+  module Base = struct
+    type 'a t = S.state -> ('a * S.state)
 
-  let return a s = (a, s)
+    let return a s = (a, s)
 
-  let bind m f s =
-    let (a, s1) = m s in
-    f a s1
+    let bind m f s =
+      let (a, s1) = m s in
+      f a s1
+  end
+
+  include Monad(Base)
 
   let get s =
     s, s
@@ -88,4 +147,31 @@ module State (S: STATE) = Monad(struct
   let modify f s =
     (), f s
 
-end)
+  let run m s =
+    m s
+
+end
+
+module Stato : STATE with type state = int = struct
+  type state = int
+end
+
+module Error : RESULT with type error = string = struct
+  type error = string
+end
+
+module I = State(Stato)
+
+open I
+
+let j = return true 2
+
+open ResultT(State(Stato))(Error)
+
+let i = return true
+
+(* let b () =
+  let i = (return true) 2 in
+  return ()
+
+let c = b () *)
